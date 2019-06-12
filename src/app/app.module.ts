5171407735a0ea451e6cbfd015af08c8b1eb9938
Selma <anaselma1962@gmail.com>
2019-06-12 14:29:47 -0300

    adjusts in package.json and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-iimport { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
